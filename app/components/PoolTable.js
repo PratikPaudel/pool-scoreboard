0f1da27d151aedf74977989f623999ed0141c9c2
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-09-05 17:10:27 -0500

    added pool tabel in the background wip

---
+++ app/components/PoolTable.js
@@ -0,0 +1,33 @@
+// app/components/PoolTable.js
+import React from 'react';
+import './PoolTable.css';
+
+const PoolTable = () => {
+        return (
+            <div className="pool-table w-full h-full fixed top-0 left-0 z-0 blur-lg">
+                    <div className="table-carpet"></div>
+                    <div className="table-border"></div>
+
+                    {/* pockets */}
+                    <div className="pocket top-left"></div>
+                    <div className="pocket top-center"></div>
+                    <div className="pocket top-right"></div>
+                    <div className="pocket bottom-left"></div>
+                    <div className="pocket bottom-center"></div>
+                    <div className="pocket bottom-right"></div>
+
+                    {/* holes */}
+                    <div className="hole top-left"></div>
+                    <div className="hole top-center"></div>
+                    <div className="hole top-right"></div>
+                    <div className="hole bottom-left"></div>
+                    <div className="hole bottom-center"></div>
+                    <div className="hole bottom-right"></div>
+
+                    {/* cue */}
+                    <div className="billiard-cue"></div>
+            </div>
+        );
+};
+
+export default PoolTable;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+// app/page.js
 'use client';
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
+import PoolTable from './components/PoolTable'; // Import the PoolTable component
 
 import '/public/global.css';
+
 const Home = () => {
     const [scores, setScores] = useState({ pratik: 0, nick: 0 });
 
@@ -24,42 +27,43 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="py-4 md:py-10 flex flex-col gap-8 sm:gap-10 items-center">
+        <div className="relative py-4 md:py-10 flex flex-col gap-8 sm:gap-10 items-center">
+            <PoolTable /> {/* Add the PoolTable component */}
             <h1 className="text-4xl sm:text-5xl md:text-6xl text-center font-extrabold">
                 <span className="text-blue-600">Pool Game Score Tracker </span>
             </h1>
             <div className={"flex flex-row gap-4 sm:gap-6 items-center"}>
-            <a href="" target="_blank" rel="noopener noreferrer">
-                <div className="card">
-                    <div className="wrapper">
-                        <div className="text-center">
-                            <h1> {scores.pratik} </h1>
+                <a href="" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <div className="wrapper">
+                            <div className="text-center">
+                                <h1 className="text-6xl background-animate bg-gradient-to-r from-indigo-500 via-green-500 to-pink-500 bg-clip-text flex justify-center items-center content-center w-full text-transparent text-6xl select-none py-10"> {scores.pratik} </h1>
+                            </div>
+                            <img src="/pratik.png"
+                                 className="cover-image" alt="Pratik Paudel"/>
                         </div>
-                        <img src="/pratik.png"
-                             className="cover-image" alt="Pratik Paudel"/>
+                        <img src="/title-pratik.png" className="title"
+                             alt="Pratik Paudel"/>
+                        <img src="/hover-one.png"
+                             className="character" alt="Pratik Paudel"/>
                     </div>
-                    <img src="/title-pratik.png" className="title"
-                         alt="Pratik Paudel"/>
-                    <img src="/hover-one.png"
-                         className="character" alt="Pratik Paudel"/>
-                </div>
-            </a>
+                </a>
 
-            <a href="" target="_blank" rel="noopener noreferrer">
-                <div className="card">
-                    <div className="wrapper">
-                        <div className="text-center">
-                            <h3 className="text-lg font-semibold text-blue-600 mb-2">{scores.nick}</h3>
+                <a href="" target="_blank" rel="noopener noreferrer">
+                    <div className="card">
+                        <div className="wrapper">
+                            <div className="text-center">
+                                <h3 className="text-6xl background-animate bg-gradient-to-r from-indigo-500 via-green-500 to-pink-500 bg-clip-text flex justify-center items-center content-center w-full text-transparent text-6xl select-none py-10">{scores.nick}</h3>
+                            </div>
+                            <img src="/nick.png"
+                                 className="cover-image" alt=""/>
                         </div>
-                        <img src="/nick.png"
-                             className="cover-image" alt=""/>
+                        <img src="/title-nick.png" className="title"
+                             alt="Nick Batcheller"/>
+                        <img src="/hover-two.png"
+                             className="character" alt="Nick Batcheller"/>
                     </div>
-                    <img src="/title-nick.png" className="title"
-                         alt="Nick Batcheller"/>
-                    <img src="/hover-two.png"
-                         className="character" alt="Nick Batcheller"/>
-                </div>
-            </a>
+                </a>
             </div>
             <div className="w-full max-w-md">
                 <button
