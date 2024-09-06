f68b1b9e1db68ec243104dfd0602d0624c74dd65
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-09-06 02:35:31 -0500

    added signout

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 ﻿// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import { getAuth } from 'firebase/auth';
 // Web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
