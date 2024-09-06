575d4822080e17a712a3341302e505a62f8f9998
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-09-06 02:16:16 -0500

    updated header paddings

--- app/components/header.js
+++ app/components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header className="w-full flex justify-end p-20 fixed top-0 right-0 z-10">
+        <header className="w-full flex justify-end p-12 fixed top-0 right-0 z-10">
             <button className="bg-green-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-700 transition duration-300 ease-in-out">
                 <a href="/admin">Admin Login</a>
             </button>
